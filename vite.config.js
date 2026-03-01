247c31106fe838e45fe23f536b296bedc99def78
felipeaguileraaguilera-hue <felipe.aguilera.aguilera@gmail.com>
2026-03-01 23:47:33 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/combustible/',  // Cambiar si se despliega en otro path
+  base: '/',  // Cambiar si se despliega en otro path
   server: {
     port: 3000,
     host: true
