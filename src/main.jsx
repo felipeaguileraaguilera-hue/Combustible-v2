f751e63b4b3c2325e8d84f7055275a0150e8dd40
felipeaguileraaguilera-hue <felipe.aguilera.aguilera@gmail.com>
2026-03-02 23:35:07 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,18 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './styles/index.css'
 
+// SSO: detectar token del portal
+const hash = window.location.hash
+if (hash.includes('access_token=')) {
+  const params = new URLSearchParams(hash.substring(1))
+  const access_token = params.get('access_token')
+  const refresh_token = params.get('refresh_token')
+  if (access_token && refresh_token) {
+    supabase.auth.setSession({ access_token, refresh_token })
+    window.location.hash = '' // limpiar URL
+  }
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
